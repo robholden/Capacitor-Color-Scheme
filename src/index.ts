7923b9da9815c43cee3f5dc70ae9675449940813
Robert Holden <rob.holden@live.co.uk>
2022-04-07 21:34:06 +0100

    Added ios to index

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import type { ColorSchemePlugin } from './definitions';
 
 const ColorScheme = registerPlugin<ColorSchemePlugin>('ColorScheme', {
   web: () => import('./web').then(m => new m.ColorSchemeWeb()),
+  ios: () => import('./web').then(m => new m.ColorSchemeWeb()),
 });
 
 export * from './definitions';
